2dcb06760a8f7ae58902d59df4194a74c473cd2a
jang-sw <jsw118536@gmail.com>
2024-06-03 02:46:45 +0900

    최초커밋

---
+++ src/main/java/com/example/demo/SpringReactorWebsocketDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactorWebsocketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactorWebsocketDemoApplication.class, args);
+	}
+
+}
+ 
